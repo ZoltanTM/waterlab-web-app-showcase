e3cda4f68f66ae7eb9214ecd89fe35982661aac8
LuciusAlexander <lucian.alexcoseru@yahoo.com>
2021-01-15 23:38:07 +0100

    Attempt 2 for Gh Pages fix: Created 404.html and redirected path in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // import logo from './logo.svg';
 // import './App.css';
@@ -8,20 +8,42 @@ import Layout from './components/layout/Layout';
 // import ScrollTop from './components/Layout/ScrollTop';
 // import LogoutModal from './components/Layout/LogoutModal';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Layout />
-      </div>
-    </BrowserRouter>
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="App">
+//         <Layout />
+//       </div>
+//     </BrowserRouter>
 
-    // <div className="App">
-    //     <PageWrapper />
-    //     <ScrollTop />    
-    //     <LogoutModal />
-    // </div>
-  );
+//     // <div className="App">
+//     //     <PageWrapper />
+//     //     <ScrollTop />    
+//     //     <LogoutModal />
+//     // </div>
+//   );
+// }
+
+class App extends React.Component {  
+  // title = 'waterlab';
+
+  componentDidMount() {
+    let path = localStorage.getItem('path');
+    if(path) {
+      localStorage.removeItem('path');
+      this.router.navigate([path]);
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Layout />
+        </div>
+      </Router>  
+    );
+  }
 }
 
 export default App;
