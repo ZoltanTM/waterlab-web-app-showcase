8eb1e7646a9dcc50c52cb712e52ba0ba0b172cd0
LuciusAlexander <lucian.alexcoseru@yahoo.com>
2021-01-15 23:47:26 +0100

    Reversed changes back to pre-Gh attempted fix

--- src/App.js
+++ src/App.js
@@ -1,49 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 // import logo from './logo.svg';
 // import './App.css';
 import Layout from './components/layout/Layout';
-// import PageWrapper from './components/Layout/PageWrapper';
-// import ScrollTop from './components/Layout/ScrollTop';
-// import LogoutModal from './components/Layout/LogoutModal';
 
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <div className="App">
-//         <Layout />
-//       </div>
-//     </BrowserRouter>
-
-//     // <div className="App">
-//     //     <PageWrapper />
-//     //     <ScrollTop />    
-//     //     <LogoutModal />
-//     // </div>
-//   );
-// }
-
-class App extends React.Component {  
-  // title = 'waterlab';
-
-  componentDidMount() {
-    let path = localStorage.getItem('path');
-    if(path) {
-      localStorage.removeItem('path');
-      this.router.navigate([path]);
-    }
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Layout />
-        </div>
-      </Router>  
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Layout />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
