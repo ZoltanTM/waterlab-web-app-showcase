b197a8212815d6d2ffc22ca4f89fd043c4c5e7af
LuciusAlexander <lucian.alexcoseru@yahoo.com>
2021-01-18 11:55:13 +0100

    Modified Layout path in App.js for Netlify

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 // import logo from './logo.svg';
 // import './App.css';
-import Layout from './components/layout/Layout';
+import Layout from 'components/layout/Layout';
 
 function App() {
   return (
